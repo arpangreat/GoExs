81edd686879bd58e7facf48a20230b9c58f7476e
arpangreat <arpanthegreat41@gmail.com>
2020-08-15 21:35:40 +0530

    Committing with pointer reciever method

--- pointersMethodDemo/pointersMethod.go
+++ pointersMethodDemo/pointersMethod.go
@@ -20,10 +20,17 @@ func (c car) mph() float64 {
 	return float64(c.gas_pedal) * (c.top_speed_kmh / uSixteenMax / kmh_multiple)
 }
 
+func (c *car) new_top_speed(newspeed float64) {
+	c.top_speed_kmh = newspeed
+}
+
 func main() {
 	a_car := car{gas_pedal: 2234, brake_pedal: 0, steering_wheel: 12561, top_speed_kmh: 225.0}
 
 	fmt.Println(a_car.gas_pedal)
+	fmt.Println(a_car.kmh())
+	fmt.Println(a_car.mph())
+	a_car.new_top_speed(500)
 	fmt.Println(a_car.kmh())
 	fmt.Println(a_car.mph())
 }
